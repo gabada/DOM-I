776907661d966890b6f0e5eef346b8a394ff84f7
Gill Abada <gill@callifi.com>
2019-01-28 18:10:30 -0500

    add a warning message to wait for the top of the minute.

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,5 +1,9 @@
 const ms = document.querySelectorAll(".digit")
 const refresh = setInterval(refreshTimer, 10);
+const digits = document.querySelector('.digits');
+
+const newDiv = document.createElement('p');
+newDiv.textContent = "Wait for the top of the minute for the counter to start again...This is V1 and I am lazy"
 
 
 function refreshTimer() {
@@ -9,10 +13,13 @@ function refreshTimer() {
         ms[4].textContent = 0;
         ms[3].style.color="red";
         ms[4].style.color="red";
+        digits.append(newDiv);
     } else {
         ms[3].textContent = 0;
         ms[4].textContent = d.getSeconds();
         ms[3].style.color="black";
         ms[4].style.color="black";
+        newDiv.style.display = "none";
     }
 }
+
