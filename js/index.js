841ee76dca91da6172a2dcef25a392235dd28793
Gill Abada <gill@callifi.com>
2019-01-28 15:27:20 -0500

    make nav bar links green.

--- js/index.js
+++ js/index.js
@@ -50,6 +50,7 @@ navLinks[3].textContent = siteContent["nav"]["nav-item-4"];
 navLinks[4].textContent = siteContent["nav"]["nav-item-5"];
 navLinks[5].textContent = siteContent["nav"]["nav-item-6"];
 
+navLinks.forEach(link => link.style.color = "green");
 // CTA
 const ctaHeader = document.querySelector("h1");
 ctaHeader.textContent = siteContent["cta"].h1;
