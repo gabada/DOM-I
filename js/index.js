081d85924aec342d043cabf6030852a22c85b2c3
Gill Abada <gill@callifi.com>
2019-01-28 14:49:26 -0500

    change to double quotes, and complete CTA section.

--- js/index.js
+++ js/index.js
@@ -39,13 +39,23 @@ const siteContent = {
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"]);
+logo.setAttribute("src", siteContent["nav"]["img-src"]);
 
 // NAV Links
 const navLinks = document.querySelectorAll("a");
-navLinks[0].textContent = siteContent['nav']['nav-item-1'];
-navLinks[1].textContent = siteContent['nav']['nav-item-2'];
-navLinks[2].textContent = siteContent['nav']['nav-item-3'];
-navLinks[3].textContent = siteContent['nav']['nav-item-4'];
-navLinks[4].textContent = siteContent['nav']['nav-item-5'];
-navLinks[5].textContent = siteContent['nav']['nav-item-6'];
+navLinks[0].textContent = siteContent["nav"]["nav-item-1"];
+navLinks[1].textContent = siteContent["nav"]["nav-item-2"];
+navLinks[2].textContent = siteContent["nav"]["nav-item-3"];
+navLinks[3].textContent = siteContent["nav"]["nav-item-4"];
+navLinks[4].textContent = siteContent["nav"]["nav-item-5"];
+navLinks[5].textContent = siteContent["nav"]["nav-item-6"];
+
+// CTA
+const ctaHeader = document.querySelector("h1");
+ctaHeader.textContent = siteContent["cta"].h1;
+
+const ctaBtn = document.querySelector("button");
+ctaBtn.textContent = siteContent["cta"].button;
+
+const ctaImg = document.getElementById("cta-img");
+ctaImg.setAttribute("src", siteContent["cta"]["img-src"]);
